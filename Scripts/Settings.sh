341b9fe9a914cb84aa53fa9db639620648fe9891
VIKING <viking.yfy@qq.com>
2023-12-03 18:21:58 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -9,5 +9,3 @@ sed -i "s/hostname='.*'/hostname='$CWRT_NAME'/g" ./package/base-files/files/bin/
 #修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
-#修改默认时间格式
-sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
