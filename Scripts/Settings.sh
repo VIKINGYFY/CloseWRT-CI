d038b76f57c40c9900c6ccd0b03c0134d15a9cbb
VIKING <viking.yfy@qq.com>
2023-12-03 20:37:27 +0800

    统一命名

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$CWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$CWRT_IP/g" ./package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" ./package/base-files/files/bin/config_generate
 #修改默认主机名
-sed -i "s/hostname='.*'/hostname='$CWRT_NAME'/g" ./package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" ./package/base-files/files/bin/config_generate
 #修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
