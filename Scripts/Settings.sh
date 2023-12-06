aab75e4b1fc95b0ab8b1bd6dfb3a6076e609c04e
VIKING <viking.yfy@qq.com>
2023-12-07 00:35:44 +0800

    优化 工具链缓存

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#添加编译日期标识
-VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
-awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
@@ -15,8 +12,12 @@ sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
-WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
+#添加编译日期标识
+VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
+awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
+
 #修改默认WIFI名
+WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
 sed -i "23s/ssid=\".*\"/ssid=\"$WRT_WIFI\"/" $WIFI_FILE
 sed -i "29s/ssid=\".*\"/ssid=\"$WRT_WIFI-5G\"/" $WIFI_FILE
 
