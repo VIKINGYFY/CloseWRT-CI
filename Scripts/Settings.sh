b54e84cdb54f0047806c7ae1db45875d7bef08a2
VIKING <viking.yfy@qq.com>
2023-12-12 18:10:27 +0800

    更新 Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -2,9 +2,6 @@
 
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#添加编译日期标识
-#sed -i "s/DISTRIB_TARGET='\(.*\)'/DISTRIB_TARGET='\1 $WRT_REPO-$WRT_DATE'/g" ./package/base-files/files/etc/openwrt_release
-sed -i "s/luciversion = \"\(.*\)\"/luciversion = \"\1 $WRT_REPO-$WRT_DATE\"/g" ./feeds/luci/modules/luci-lua-runtime/src/mkversion.sh
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
@@ -14,6 +11,10 @@ sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 #修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
+
+#添加编译日期标识
+VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
+awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
 
 #修改默认WIFI名
 WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
