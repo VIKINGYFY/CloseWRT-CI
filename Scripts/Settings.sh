7410a17246560b3f2978a820aa2bc7478430ef08
VIKING <viking.yfy@qq.com>
2023-12-13 21:51:29 +0800

    优化 部分提示

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -12,14 +12,14 @@ sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
-#添加编译日期标识
-VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
-awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
-
 #修改默认WIFI名
 WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
 sed -i "/htbsscoex=\"1\"/{n; s/ssid=\".*\"/ssid=\"$WRT_WIFI\"/}" $WIFI_FILE
 sed -i "/htbsscoex=\"0\"/{n; s/ssid=\".*\"/ssid=\"$WRT_WIFI-5G\"/}" $WIFI_FILE
+
+#添加编译日期标识
+VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
+awk -v wrt_repo="$WRT_REPO" -v wrt_date="$WRT_DATE" '{ gsub(/(\(luciversion \|\| \047\047\))/, "& + (\047 / "wrt_repo"-"wrt_date"\047)") } 1' $VER_FILE > temp.js && mv -f temp.js $VER_FILE
 
 #配置文件修改
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
