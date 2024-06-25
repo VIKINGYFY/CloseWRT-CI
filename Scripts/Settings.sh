6655051bbb94b9bac9feb72723a9730057445f3a
VIKING <viking.yfy@qq.com>
2024-06-25 22:43:49 +0800

    新增手调插件包功能

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -27,3 +27,8 @@ echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 echo "CONFIG_MTK_MEMORY_SHRINK=$([[ $WRT_SHRINK == "true" ]] && echo "y" || echo "n")" >> ./.config
 echo "CONFIG_MTK_MEMORY_SHRINK_AGGRESS=$([[ $WRT_SHRINK == "true" ]] && echo "y" || echo "n")" >> ./.config
+
+#手动调整的插件
+if [ -n "$WRT_PACKAGE" ]; then
+	echo "$WRT_PACKAGE" >> ./.config
+fi
