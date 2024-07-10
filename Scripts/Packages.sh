d3ed10d63df647fa750b5bd5b1576a7d230b3d10
VIKING <viking.yfy@qq.com>
2024-07-10 09:59:31 +0800

    新增酷猫主题

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -25,12 +25,13 @@ UPDATE_PACKAGE "design" "gngpp/luci-theme-design" "js"
 UPDATE_PACKAGE "design-config" "gngpp/luci-app-design-config" "master"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "master"
+UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
-UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
+UPDATE_PACKAGE "advancedplus" "sirpdboy/luci-app-advancedplus" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
