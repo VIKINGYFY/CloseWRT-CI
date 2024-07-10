18657d032b9d7c16a4555ce82345c94b0b16c38b
VIKING <viking.yfy@qq.com>
2024-07-10 22:29:10 +0800

    移除过时插件

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -21,10 +21,8 @@ UPDATE_PACKAGE() {
 }
 
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "js"
-UPDATE_PACKAGE "design-config" "0x676e67/luci-app-design-config" "master"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
-UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "master"
+UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "js"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
