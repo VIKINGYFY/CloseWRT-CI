4a6dec5169653d54cf5b4d9b1aba1e0c389fa183
VIKING <viking.yfy@qq.com>
2024-07-20 12:55:15 +0800

    luci-app-tailscale

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -31,7 +31,6 @@ UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
 UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
-UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
