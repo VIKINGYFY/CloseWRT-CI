4fc635e605d807ad976013961f8a327d77f40d7c
VIKING <viking.yfy@qq.com>
2024-09-26 19:26:32 +0800

    调整23.05默认设置

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -19,18 +19,28 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 
 #配置文件修改
+echo "CONFIG_PACKAGE_luci=y" >> ./.config
+echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 echo "CONFIG_MTK_MEMORY_SHRINK=$([[ $WRT_ADJUST == "true" ]] && echo "y" || echo "n")" >> ./.config
 echo "CONFIG_MTK_MEMORY_SHRINK_AGGRESS=$([[ $WRT_ADJUST == "true" ]] && echo "y" || echo "n")" >> ./.config
 
-#23.05专用
-if [[ $WRT_BRANCH == *"23.05"* ]]; then
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-openclash=n" >> ./.config
-fi
-
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
 	echo "$WRT_PACKAGE" >> ./.config
+fi
+
+#23.05专用
+if [[ $WRT_BRANCH == *"23.05"* ]]; then
+	sed -i '/luci-app-openclash/d' ./.config
+	sed -i '/luci-app-upnp/d' ./.config
+	sed -i '/miniupnpd/d' ./.config
+
+	echo "CONFIG_PACKAGE_luci-app-openclash=n" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-upnp=n" >> ./.config
+	echo "CONFIG_PACKAGE_miniupnpd=n" >> ./.config
+
+	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-upnp-mtk-adjust=y" >> ./.config
 fi
