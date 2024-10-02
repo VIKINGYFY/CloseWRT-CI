8bffb9a00151bc69d9091b2247858023dd607cf2
VIKING <viking.yfy@qq.com>
2024-10-02 15:54:27 +0800

    调整23.05默认设置

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -34,7 +34,13 @@ fi
 #23.05专用
 if [[ $WRT_BRANCH == *"23.05"* ]]; then
 	sed -i '/luci-app-openclash/d' ./.config
+	sed -i '/luci-app-upnp/d' ./.config
+	sed -i '/miniupnpd/d' ./.config
+
+	echo "CONFIG_PACKAGE_luci-app-openclash=n" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-upnp=n" >> ./.config
+	echo "CONFIG_PACKAGE_miniupnpd=n" >> ./.config
 
 	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-openclash=n" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-upnp-mtk-adjust=y" >> ./.config
 fi
