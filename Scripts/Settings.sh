d22c8adc8f8a6958149a3e8821a3d87f89331842
VIKING <viking.yfy@qq.com>
2025-06-28 09:50:32 +0800

    插件调整

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -31,8 +31,3 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 if [ -n "$WRT_PACKAGE" ]; then
 	echo -e "$WRT_PACKAGE" >> ./.config
 fi
-
-#闭源MTK平台调整
-sed -i '/luci-app-eqos/d' ./.config
-sed -i '/luci-app-mtk/d' ./.config
-sed -i '/wifi-profile/d' ./.config
